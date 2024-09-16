4b3e9eae954312c0d8149765efe6159235f7c387
paulsheo <113318941+paulsheo@users.noreply.github.com>
2024-09-15 20:57:13 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+
+
+function App() {
+ 
+  return (
+   <div className="app">
+    <section id="content">
+      <header>
+        <div>
+          <h1>My Tunes</h1>
+
+          <div>
+            <button className="icon-button">
+              <span>&#128722;</span></button>
+              <span className="badge">3</span>
+            <button className="icon-button">
+            <span className="badge">3</span>
+              <span>&#129293;</span></button>
+          </div>
+        </div>
+
+<div>
+  <nav>
+
+ 
+   
+  
+
+  </nav>
+
+    </div>
+
+      </header>
+
+      <main>
+       
+        <div className="product">
+
+          <img src="https://upload.wikimedia.org/wikipedia/commons/d/d9/191125_Taylor_Swift_at_the_2019_American_Music_Awards.png"></img>
+       
+        </div>
+
+           <details>
+      <summary>
+     Reputation
+        </summary>
+      <div>1. I Did Something Bad</div>
+      <div>2. Delicate</div>
+      <div>3. So it Goes...</div>
+      <div>4. Dont Blame Me</div>
+      </details>
+
+      <details>
+      <summary>
+     Lover
+        </summary>
+      <div>1. I Forgot That You Existed</div>
+      <div>2. Lover</div>
+      <div>3. The Archer</div>
+      <div>4. Cruel Summer</div>
+      </details>
+    
+    
+
+
+
+
+
+      </main>
+    </section>
+    <footer>
+    <div>
+   
+    </div>
+
+    <div>
+    
+    </div>
+
+    <div>
+    
+    </div>
+    </footer>
+
+   </div>
+  );
+}
+
+export default App
